cec2cbc84061a77f93fbe92237f38fd46f102293
DaniMasterLucky <danielsilvaloew@gmail.com>
2023-04-19 21:45:14 -0300

    Subindo arquivos de variaveis

---
+++ intex.js
@@ -0,0 +1,42 @@
+//Primeiro exércicio  de interpretação atividade 1
+
+/*Resposta: nas primeiras linhas, irá aparecer no console apenas o let b = 10,
+pois no console.log especificou apenas a variavel b = 10 para aparecer.
+Já nas linhas finais, foi alterado a variavel b para 5, e agora o console.log
+está pedindo tanto a variavel alterada como a let a = 10, que no final o
+resultado será 10 e 5.*/
+
+
+
+ //Atividade 2
+
+ /* Resposta: bom, será o seguinte, let a e b tem valores, 10 e 20, e logo
+ abaixo, há certos valores iguais, ou seja:
+
+ c = a > 10
+ b = c > 10
+ a = b > 10 
+
+  e aparecerá 10,10,10 no console*/ 
+
+//Atividade 3 
+
+/* Como pode ver abaixo,aparecerá o pop up lhe pergutando as questões,
+Após isso, o alert(`Voce recebe ${t/p} por hora`) falará quanto você ganha
+por hora.
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`) */
+
+// primeira atividade prática 
+
+let nome
+let idade        // Ambos foram impressos por conta do comando let//
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+nome = prompt
+idade = prompt 
+
